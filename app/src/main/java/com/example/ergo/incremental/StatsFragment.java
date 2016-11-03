448ee88771311a878f76b0de9c06e0fce7e3f640
tiberiusuciu <tiberiu.cristian.suciu@gmail.com>
2016-11-03 19:55:25 -0400

    Placed evrything toghther to have a first non-usable versionb

---
+++ app/src/main/java/com/example/ergo/incremental/StatsFragment.java
@@ -0,0 +1,20 @@
+package com.example.ergo.incremental;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ergo on 03/11/16.
+ */
+
+public class StatsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.stats_fragment, container, false);
+    }
+}
