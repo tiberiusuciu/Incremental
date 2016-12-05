321dafc632b2036430fbdf643f4fd3e970814da0
tiberiusuciu <tiberiu.cristian.suciu@gmail.com>
2016-12-04 23:05:06 -0500

    Tapping work button instrumented test

---
+++ app/src/androidTest/java/com/example/ergo/incremental/TappingWorkButtonTest.java
@@ -0,0 +1,40 @@
+package com.example.ergo.incremental;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TappingWorkButtonTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void tappingWorkButtonTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.button), withText("Work"),
+                        withParent(withId(R.id.fragment)),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        onView(withId(R.id.codeText)).check(matches(withText("Remaining Code: 1")));
+    }
+
+}
